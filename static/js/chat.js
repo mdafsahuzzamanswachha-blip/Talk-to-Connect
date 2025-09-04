88f5d92660d9b2fbb66937bf75106bfad93d211d
mdafsahuzzamanswachha-blip <mdafsahuzzamanswachha@gmail.com>
2025-09-04 22:31:07 +0600

    Update chat.js

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,1 +1,15 @@
+const socket = io();
 
+function sendMessage() {
+  const input = document.getElementById('message-input');
+  const message = input.value;
+  socket.emit('send_message', { username: 'Anonymous', message });
+  input.value = '';
+}
+
+socket.on('receive_message', data => {
+  const box = document.getElementById('chat-box');
+  const msg = document.createElement('div');
+  msg.textContent = `${data.username}: ${data.message}`;
+  box.appendChild(msg);
+});
