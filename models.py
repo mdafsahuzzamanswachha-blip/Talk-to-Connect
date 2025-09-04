a12ae4c9364b529387bd73fb2368531387b169b4
mdafsahuzzamanswachha-blip <mdafsahuzzamanswachha@gmail.com>
2025-09-04 21:59:17 +0600

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,1 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
-db = SQLAlchemy()
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
