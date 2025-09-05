035e2cb0e51d8668c4114740eb3a4e4c02dc67c5
mdafsahuzzamanswachha-blip <mdafsahuzzamanswachha@gmail.com>
2025-09-05 16:21:49 +0600

    Update auth.js

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const forms = document.querySelectorAll("form");
 
+  forms.forEach(form => {
+    form.addEventListener("submit", (e) => {
+      const submitButton = form.querySelector("button[type='submit']");
+      submitButton.disabled = true;
+      submitButton.textContent = "Please wait...";
+    });
+  });
+});
